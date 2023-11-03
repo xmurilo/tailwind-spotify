5ab8f6dbb850b11353dc6775de4460a92b7101f2
Murilo <dixmurilox@gmail.com>
2023-11-03 16:16:00 -0300

    feat adding breakpoints for responsive

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,20 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
-  plugins: [],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [
+  ],
+   
 }
 export default config
