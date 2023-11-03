1fd9f8d58fcbfca00dc28239af73482bece0e103
Murilo <dixmurilox@gmail.com>
2023-11-03 15:38:36 -0300

    feat add component of infoArtists

---
+++ src/app/infoArtists.tsx
@@ -0,0 +1,46 @@
+import imgAkari from '../../public/akari-img.jpg';
+import imgPolyphia from '../../public/foto-album-polyphia.jpg';
+import imgJapa from '../../public/foto-album-japa.jpg';
+import imgNonly from '../../public/nonly-stay-with-me.jpg';
+import imgTwentyOnePilots from '../../public/ride-twenty-one-pilots.jpg';
+import imgLilPeep from '../../public/foto-album-lil-peep.jpg';
+
+export const infoArtists = [
+  {
+    id: 1,
+    nameMusic: 'Akari',
+    artist: 'Soushi Sakiyama ',
+    img: imgAkari,
+  },
+  {
+    id: 2,
+    nameMusic: 'Playboy',
+    artist: 'Japa',
+    img: imgJapa,
+  },
+  {
+    id: 3,
+    nameMusic: 'Stay With Me',
+    artist: 'Nonly',
+    img: imgNonly,
+  },
+  {
+    id: 4,
+    nameMusic: 'Ride',
+    artist: 'Twenty One Pilots',
+    img: imgTwentyOnePilots,
+  },
+  {
+    id: 5,
+    nameMusic: 'Save that Shit',
+    artist: 'Lil Peep',
+    img: imgLilPeep,
+  },
+
+  {
+    id: 6,
+    nameMusic: 'Ego Death',
+    artist: 'Polyphia',
+    img: imgPolyphia,
+  },
+];
