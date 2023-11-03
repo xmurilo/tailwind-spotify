cd8ecfb469b80dbb977db2b8c3ab6c3fcda43284
Murilo <dixmurilox@gmail.com>
2023-11-03 15:38:14 -0300

    feat add component of collectionMix

---
+++ src/app/collectionMix.tsx
@@ -0,0 +1,31 @@
+import imgMixtape1 from '../../public/mix-1.jpg';
+import imgMixtape2 from '../../public/mix-2.jpg';
+import imgMixtape3 from '../../public/mix-3.jpg';
+import discoverWeekImg from '../../public/discover-week.jpg';
+
+export const collectionMix = [
+  {
+    id: 1,
+    img: imgMixtape1,
+    title: 'Mix do Dia 1',
+    subTitle: 'YUNG LIXO, Leozin, Class A e mais',
+  },
+  {
+    id: 2,
+    img: imgMixtape2,
+    title: 'Mix do Dia 2',
+    subTitle: '1nonly, lilbubblegum, suburban e mais',
+  },
+  {
+    id: 3,
+    img: imgMixtape3,
+    title: 'Mix do Dia 3',
+    subTitle: 'Lil Peep, Yunggoth, Lil Nas X e mais',
+  },
+  {
+    id: 4,
+    img: discoverWeekImg,
+    title: 'Discover Weekly',
+    subTitle: 'A tua compilação semanal de música nova.',
+  },
+];
