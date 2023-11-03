3f2f980582293072f677a7529e8a9181c2ff6e1b
Murilo <dixmurilox@gmail.com>
2023-11-03 17:37:18 -0300

    feat responsive sideBar

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -56,7 +56,7 @@ export function SideBar() {
   ));
 
   return (
-    <aside className='w-72 bg-zinc-950 p-6'>
+    <aside className='desktop:w-72 bg-zinc-950 p-6'>
       <div className='flex items-center gap-2'>
         <div className='w-3 h-3 bg-red-500 rounded-full' />
         <div className='w-3 h-3 bg-yellow-500 rounded-full' />
